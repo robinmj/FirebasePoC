e0fbeb194d9c70ad963c581d60cdd3e99e8982a2
Robin <robin.m.j@gmail.com>
2016-02-11 13:17:22 -0700

    added logging tag

--- app/src/main/java/com/example/firebasepoc/App.java
+++ app/src/main/java/com/example/firebasepoc/App.java
@@ -8,6 +8,8 @@ import com.firebase.client.Firebase;
  * Created by robin on 2/10/16.
  */
 public class App extends Application {
+    public static final String TAG = "FirebasePoC";
+
     @Override
     public void onCreate() {
         super.onCreate();
